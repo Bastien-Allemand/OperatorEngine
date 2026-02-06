de27bf1b74baa03a4679f3fccfebd58e1d12fcee
NejivoltH <lbruchet@gaming.tech>
2026-02-06 14:44:12 +0100

    **[FIX]**
    fix du ddsTextureLoader

--- src/Render/DDSTextureLoader.cpp
+++ src/Render/DDSTextureLoader.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 //--------------------------------------------------------------------------------------
 // File: DDSTextureLoader.cpp
 //
@@ -1269,8 +1270,10 @@ static HRESULT CreateD3DResources12(
 		texDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
 		texDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
 
+
+        D3D12_HEAP_PROPERTIES heapPropertiesDefault = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
 		hr = device->CreateCommittedResource(
-			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+			&heapPropertiesDefault,
 			D3D12_HEAP_FLAG_NONE,
 			&texDesc,
 			D3D12_RESOURCE_STATE_COMMON,
@@ -1288,10 +1291,12 @@ static HRESULT CreateD3DResources12(
 			const UINT num2DSubresources = texDesc.DepthOrArraySize * texDesc.MipLevels;
 			const UINT64 uploadBufferSize = GetRequiredIntermediateSize(texture.Get(), 0, num2DSubresources);
 
+            D3D12_HEAP_PROPERTIES heapPropertiesUpload = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+            D3D12_RESOURCE_DESC pDesc = CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize);
 			hr = device->CreateCommittedResource(
-				&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+				&heapPropertiesUpload,
 				D3D12_HEAP_FLAG_NONE,
-				&CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize),
+				&pDesc,
 				D3D12_RESOURCE_STATE_GENERIC_READ,
 				nullptr,
 				IID_PPV_ARGS(&textureUploadHeap));
@@ -1302,14 +1307,17 @@ static HRESULT CreateD3DResources12(
 			}
 			else
 			{
-				cmdList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(texture.Get(),
-					D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST));
+                D3D12_RESOURCE_BARRIER pBarriers1 =  CD3DX12_RESOURCE_BARRIER::Transition(texture.Get(),
+                                                    D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST);
+				cmdList->ResourceBarrier(1, &pBarriers1);
 
 				// Use Heap-allocating UpdateSubresources implementation for variable number of subresources (which is the case for textures).
 				UpdateSubresources(cmdList, texture.Get(), textureUploadHeap.Get(), 0, 0, num2DSubresources, initData);
 
-				cmdList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(texture.Get(),
-					D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE));
+                D3D12_RESOURCE_BARRIER pBarriers2 =  CD3DX12_RESOURCE_BARRIER::Transition(texture.Get(),
+                                                    D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+
+				cmdList->ResourceBarrier(1, &pBarriers2);
 			}
 		}
 	} break;
