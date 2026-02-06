072e7a04dc25f6398ff153da0558210678b6560e
NejivoltH <lbruchet@gaming.tech>
2026-02-06 15:55:41 +0100

    FIX
    TU VAS PRENDRE MON PUSH

--- src/Core/Define.h
+++ src/Core/Define.h
@@ -1,19 +1,10 @@
 #pragma once
 
-
-// Library
-
 //#pragma comment(lib, "winmm.lib")
 //#pragma comment(lib, "ws2_32.lib")
 #define WIN32_LEAN_AND_MEAN 
 #include <windows.h>
-#include <wrl.h>        // Pour Microsoft::WRL::ComPtr
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <cstdint>
-#include <string>
 #include <stdexcept>
-
 
 using int8 = __int8;
 using int16 = __int16;
@@ -37,32 +28,3 @@ using float64 = double;
 #define Vector3 DirectX::XMFLOAT3 
 #define Quaternion DirectX::XMFLOAT4
 #define Matrix DirectX::XMMATRIX
-
-#define SAFE_RELEASE(p) if (p) { (p)->Release(); (p) = nullptr; }
-
-// CATH INITIALISATION OR MEMORY GPU ERROR
-inline void ThrowIfFailed(HRESULT hr) {
-	if (FAILED(hr)) {
-		// Dans un vrai moteur, on loggerait l'erreur ici
-	#ifdef _DEBUG
-			OutputDebugStringA("DirectX Error Detected!\n");
-			DebugBreak();
-	#endif
-			throw std::runtime_error("DirectX Error");
-	}
-}
-
-// ALIGNEMENT DES CONSTANT BUFFER
-#define D3D12_CONSTANT_BUFFER_ALIGN(size) ((size + 255) & ~255)
-
-
-#define ALIGN_UP(size, alignment) (((size) + (alignment) - 1) & ~((alignment) - 1))
-
-
-
-// DEBUG CONFIGURATIONa
-#ifdef _DEBUG
-#define ENGINE_DEBUG 1
-#else
-#define ENGINE_DEBUG 0
-#endif
