9d78819652454cbff5582fc9c4dc159779bbee80
NejivoltH <lbruchet@gaming.tech>
2026-02-06 15:46:57 +0100

    fix
    refix define

--- src/Core/Define.h
+++ src/Core/Define.h
@@ -3,8 +3,8 @@
 
 // Library
 
-#pragma comment(lib, "winmm.lib")
-#pragma comment(lib, "ws2_32.lib")
+//#pragma comment(lib, "winmm.lib")
+//#pragma comment(lib, "ws2_32.lib")
 #define WIN32_LEAN_AND_MEAN 
 #include <windows.h>
 #include <wrl.h>        // Pour Microsoft::WRL::ComPtr
@@ -19,55 +19,48 @@ using int8 = __int8;
 using int16 = __int16;
 using int32 = __int32;
 using int64 = __int64;
+using uint32_t = __int32;
 
 using uint8 = unsigned __int8;
 using uint16 = unsigned __int16;
 using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
+using uint32_t = unsigned __int32;
 
 using float32 = float;
 using float64 = double;
 
 //DirectX
-
 #include <DirectXMath.h>
-using namespace DirectX;
 
+#define Vector2 DirectX::XMFLOAT2 
+#define Vector3 DirectX::XMFLOAT3 
+#define Quaternion DirectX::XMFLOAT4
+#define Matrix DirectX::XMMATRIX
 
-XMFLOAT3 d;
-
-
-// Maths
-namespace ope {
-
-	template <typename T> union Vector2;
-	union Vector3f32;
-
-	using Vector2i8 = Vector2<int8>;
-	using Vector2i16 = Vector2<int16>;
-	using Vector2i32 = Vector2<int32>;
-	using Vector2i64 = Vector2<int64>;
-	using Vector2ui8 = Vector2<uint8>;
-	using Vector2ui16 = Vector2<uint16>;
-	using Vector2ui32 = Vector2<uint32>;
-	using Vector2ui64 = Vector2<uint64>;
-	using Vector2f32 = Vector2<float32>;
-	using Vector2f64 = Vector2<float64>;
-}
-
+#define SAFE_RELEASE(p) if (p) { (p)->Release(); (p) = nullptr; }
 
 // CATH INITIALISATION OR MEMORY GPU ERROR
 inline void ThrowIfFailed(HRESULT hr) {
 	if (FAILED(hr)) {
 		// Dans un vrai moteur, on loggerait l'erreur ici
-		throw std::runtime_error("DirectX 12 Error: HRESULT " + std::to_string(hr));
+	#ifdef _DEBUG
+			OutputDebugStringA("DirectX Error Detected!\n");
+			DebugBreak();
+	#endif
+			throw std::runtime_error("DirectX Error");
 	}
 }
+
+// ALIGNEMENT DES CONSTANT BUFFER
+#define D3D12_CONSTANT_BUFFER_ALIGN(size) ((size + 255) & ~255)
 
 
 #define ALIGN_UP(size, alignment) (((size) + (alignment) - 1) & ~((alignment) - 1))
 
-// DEBUG CONFIGURATION
+
+
+// DEBUG CONFIGURATIONa
 #ifdef _DEBUG
 #define ENGINE_DEBUG 1
 #else
