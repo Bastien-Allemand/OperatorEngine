6f97afdde69bc1ffacc3a55c8f306dd4ea87d4a3
NejivoltH <lbruchet@gaming.tech>
2026-02-06 14:34:17 +0100

    **[ADD]**
    fichier de rendu 2d

---
+++ src/Core/Define.h
@@ -0,0 +1,54 @@
+#ifndef CORE_DEFINE_H_INCLUDED
+#define CORE_DEFINE_H_INCLUDED
+
+
+// Library
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "ws2_32.lib")
+#include <cstdint>
+
+
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using uint32 = unsigned __int32;
+using uint64 = unsigned __int64;
+
+using float32 = float;
+using float64 = double;
+
+//DirectX
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+
+
+
+
+// Maths
+namespace ope {
+
+	template <typename T> union Vector2;
+	union Vector3f32;
+
+	using Vector2i8 = Vector2<int8>;
+	using Vector2i16 = Vector2<int16>;
+	using Vector2i32 = Vector2<int32>;
+	using Vector2i64 = Vector2<int64>;
+	using Vector2ui8 = Vector2<uint8>;
+	using Vector2ui16 = Vector2<uint16>;
+	using Vector2ui32 = Vector2<uint32>;
+	using Vector2ui64 = Vector2<uint64>;
+	using Vector2f32 = Vector2<float32>;
+	using Vector2f64 = Vector2<float64>;
+}
+
+
+
+#endif
