f33ad53dd2c47254bdd62e69e753f73c5eeae526
durity42 <lchatagnon@gaming.tech>
2026-02-06 14:55:47 +0100

    REF

--- src/Engine/GameManager.h
+++ src/Engine/GameManager.h
@@ -7,25 +7,32 @@ class GameManager
 {
 public:
 	static GameManager* GetInstance();
-	
+
 	template<typename T>
 	T AddComponant(UINT entity);
+
 	template<typename C>
 	C GetComponant(UINT entity);
 
-	private:
-		static GameManager* m_instance;
+private:
+	static GameManager* m_instance;
 
-		std::vector<Componant> m_componants;
-		std::unordered_map<UINT, std::vector<int>> m_componantIndexMap; //entity
+	template<typename V>
+	static std::vector<V>& GetComponantsArray()
+	{
+		static std::vector<V> m_componants;
+		return m_componants;
+	}
+
+	std::unordered_map<UINT, std::vector<int>> m_componantIndexMap; //entity
 };
 
 template<typename T>
 inline T GameManager::AddComponant(UINT entity)
 {
 	T componant;
-	m_componants.push_back(componant);
-	m_componantIndexMap[entity].push_back(m_componants.size() - 1);
+	GetComponantsArray<T>().push_back(componant);
+	m_componantIndexMap[entity].push_back(GetComponantsArray<T>().size() - 1);
 
 	return componant;
 }
@@ -35,9 +42,12 @@ inline C GameManager::GetComponant(UINT entity)
 {
 	for (int index : m_componantIndexMap[entity])
 	{
-		if (std::is_same<C, std::remove_cvref_t<decltype(m_componants[index])>>::value)
+		auto& array = GetComponantsArray<C>();
+
+		if (index >= 0 && index < array.size())
 		{
-			return static_cast<C>(m_componants[index]);
+			return array[index];
 		}
 	}
+	return C();
 }
