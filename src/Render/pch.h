b65a5d8981b6a20779330320e3621da9e5ba526b
Bastien Allemand--Leung Pin <ballemandleungpin@gaming.tech>
2026-02-12 19:35:38 +0100

    FIX
    simple pch clean

--- src/Render/pch.h
+++ src/Render/pch.h
@@ -2,16 +2,15 @@
 #define PCH_H
 
 #include <windows.h>
-#include <iostream>
 #include "../Core/Define.h"
-#include "DirectXCollision.h"
+#include <DirectXCollision.h>
+#include "DDSTextureLoader12.h"
+
+
 #include <d3d12.h>
 #include <d3dcompiler.h>
-#include <DirectXMath.h>
 #include <dxgi1_6.h>
-#include "DDSTextureLoader12.h"
-#include "d3dx12.h"
-#include "../Core/Define.h"
+
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
